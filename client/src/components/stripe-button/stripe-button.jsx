0acd43c4f0e38030b77250ce8cf13e2412012f43
yohren00 <yohren00@gmil.com>
2020-06-25 22:07:24 +0800

    使用node.js後端，寫出sripte付款，但目前尚未有解決問題

--- client/src/components/stripe-button/stripe-button.jsx
+++ client/src/components/stripe-button/stripe-button.jsx
@@ -1,14 +1,27 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
-
+import axios from "axios";
 
 const StripeButton = ({ price }) => {
     const priceFroStripe = price * 100;
     const publishableKey = "pk_test_51GvysJD7BQfhdw00gPqi1FudlbK535VLTeZXim5XOTEtOsyxil8AVvVDB9DxuxSILWV3Y3MxlTHzfQezSBpb8l8m005dwXJ87f";
 
     const onToken = token => {
-        console.log(token);
-        alert("付款成功")
+        axios({
+            url: "payment",
+            method: "post",
+            data: {
+                amout: priceFroStripe,
+                token
+            }
+        }).then(response => {
+            alert("成功付款")
+        }).catch(error => {
+            console.log("付款失敗：", error);
+            alert(
+                "付款失敗，請確認信用卡資訊是否有誤"
+            )
+        })
     };
 
     return <>
